6639b592924038ed4286749fb5e78bafdd05afc8
RIFAT ABRAR JOWAD <rajdiprifat2004@gmail.com>
2021-08-11 01:51:45 +0600

    2problem solve

---
+++ BEGINNER/1000.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+print('Hello World!')
+'''
+Escreva a sua solução aqui
+Code your solution here
+Escriba su solución aquí
+'''
---
+++ BEGINNER/1001.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+A = int(input())
+B = int(input())
+X = A+B
+print(X)
+'''
+Escreva a sua solução aqui
+Code your solution here
+Escriba su solución aquí
+'''
